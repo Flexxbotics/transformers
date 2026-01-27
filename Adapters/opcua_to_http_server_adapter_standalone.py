3cb0ac051b7e5ca315a66d549f765f82b54d37a6
Tyler Modelski <tylerjm@flexxbotics.com>
2026-01-27 08:38:01 -0500

    more enhancements to opcua to http server adapter

--- Adapters/opcua_to_http_server_adapter_standalone.py
+++ Adapters/opcua_to_http_server_adapter_standalone.py
@@ -1,3 +1,19 @@
+"""
+    Copyright 2025 Flexxbotics, Inc.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
 import asyncio
 import json
 import os
@@ -18,7 +34,7 @@ class RestRoute:
     default_body: Optional[Dict[str, Any]] = None
 
 
-class RestAdapter:
+class OpcuaRestAdapter:
     """
     Implements the same REST call behavior as tcp_to_http_server_adapter_standalone.py:
     - command dict: {"type": str, "endpoint": str, "body": json}
@@ -96,7 +112,7 @@ class RestAdapter:
 async def main():
     # Match your TCP adapter's default idea: FLASK_CONTAINER, fallback localhost (you can change)
     flask_host = os.getenv("FLASK_CONTAINER", "http://127.0.0.1:7081")
-    rest = RestAdapter(flask_host=flask_host, timeout_s=60)
+    rest = OpcuaRestAdapter(flask_host=flask_host, timeout_s=60)
 
     server = Server()
     await server.init()
